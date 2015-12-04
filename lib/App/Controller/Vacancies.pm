6a421f670353635f5150c405858655b429bef814
umar-dzahkiev <egikalegi@gmail.com>
2015-12-04 18:12:08 +0300

    ...

--- lib/App/Controller/Vacancies.pm
+++ lib/App/Controller/Vacancies.pm
@@ -1,7 +1,6 @@
 package App::Controller::Vacancies;
 use base 'Mojolicious::Controller';
 
-
 sub list {
   my $self = shift;
   my $query =  "select *, (select count(*) from negotiations where status = 'inbox' and vacancy_id = vacancies.id ) as count_responses  from vacancies";
